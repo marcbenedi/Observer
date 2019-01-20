c7edf70b985dfa4b318098772810d3917ecc77ef
Marc Benedi <marc@marcb.pro>
2019-01-20 20:23:41 +0100

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.11"
+    id("org.jetbrains.dokka") version "0.9.17"
 }
+
 group = "pro.marcb.observer"
 version = "1.0-SNAPSHOT"
+
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit:junit:4.12")
 }
+
 repositories {
     mavenCentral()
+    jcenter()
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
---
+++ src/test/kotlin/pro/marcb/observer/SampleTest.kt
@@ -0,0 +1,11 @@
+package pro.marcb.observer
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class SampleTest {
+    @Test
+    fun testMyLanguage() {
+        assertEquals("Kotlin", "Kotlin")
+    }
+}
